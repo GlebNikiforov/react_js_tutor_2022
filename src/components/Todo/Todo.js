865497fa94ab6e5f66a5aed46fa495810fc1a79e
GlebNikiforov <99167920+GlebNikiforov@users.noreply.github.com>
2022-02-19 18:29:29 +0300

    Pagination

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,27 +1,25 @@
-import { Button, Checkbox, List, Col, Input } from 'antd';
+import { Button, Checkbox, List, Col, Input, Pagination } from 'antd';
 import React, {Component } from 'react';
 
 export default class Todo extends Component {
 
     state = {
         todoList: [],
-        curentPage : 1,
+        currentPage : 1,
         perPage : 10,
     }
 
-    componentDidMount(){
+    componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/todos')
         .then(res => res.json())
         .then(res => this.setState({todoList : res}))
-        
     }
-
     
-    handelerTodoChecked = (todoID) =>{
+    handelerTodoChecked = (todoID) => {
         let todo = JSON.parse(JSON.stringify(this.state.todoList))
 
-        todo = todo.filter((item)=>{
-            if(item.id == todoID){
+        todo = todo.filter((item) => {
+            if(item.id === todoID) {
                 item.completed = !item.completed
             }
             return item;
@@ -32,9 +30,9 @@ export default class Todo extends Component {
     handlerDeleteTodo = (todoID) => {
         let todo = JSON.parse(JSON.stringify(this.state.todoList))
 
-        todo = todo.filter((item)=>{
-            if(item.id != todoID) return item;
-            
+        todo = todo.filter((item) => {
+            if (item.id !== todoID) return item;
+            else return undefined;
         })
         this.setState({todoList : todo})
     }
@@ -44,26 +42,39 @@ export default class Todo extends Component {
         let defVariable = [{id: Math.random(), title:e.target.value, completed: false}]
         this.setState({todoList : [...this.state.todoList, ...defVariable] })
     }
+
+    handlerUpdatePages = (page, pageSize) => {
+        this.setState({currentPage: page ? page : 1, perPage: pageSize});
+    }
+
     render(){
         const {todoList} = this.state;
-        
+        const {currentPage} = this.state;
+        const {perPage} = this.state;
+
+        const firstItem = (currentPage - 1) * perPage;
+        const lastItem = currentPage * perPage;
+        const todoPageList = todoList.slice(firstItem, lastItem);
+
         return(
             <>
                 <Col span={8} style={{margin: '0 auto'}}>
                     <h3>My todo list</h3>
                     <List>
                         <Input placeholder='Добавить todo в список' onPressEnter={(e) => this.handlerAddToTodo(e)}/>
-                        {todoList.map((item,index)=>{
-                            return(
-                                    
-                                    <List.Item key={index} style={{listStyle: 'decimal', textDecoration : item.completed ? 'line-through' : 'none'}} >
-                                        <Checkbox onChange={() => this.handelerTodoChecked(item.id) } checked={item.completed ? true : false }/>
-                                        {item.title}
-                                        <Button onClick={()=> this.handlerDeleteTodo(item.id) }>Удалить</Button>
-                                    </List.Item>
+                        {todoPageList.map((item, index)=>{
+                            return(          
+                                <List.Item key={index} style={{listStyle: 'decimal', textDecoration : item.completed ? 'line-through' : 'none', display: 'flex'}} >
+                                    <div>
+                                        <Checkbox onChange={() => this.handelerTodoChecked(item.id) } checked={item.completed ? true : false } style={{marginRight: 16}}/>
+                                        <span>{item.title}</span>
+                                    </div>
+                                    <Button onClick={()=> this.handlerDeleteTodo(item.id) }>Удалить</Button>
+                                </List.Item>
                             )
                         })}
                     </List>
+                    <Pagination size="small" showSizeChanger showQuickJumper defaultCurrent={currentPage} defaultPageSize={perPage} total={todoList.length} onChange={this.handlerUpdatePages} style={{paddingBottom: 16}} />
                 </Col>
             </>
         )
